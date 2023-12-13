40842bc02a32795a3275d96da605dcd870df0445
03chrisk <chris.kobriger@gmail.com>
2023-12-13 23:12:47 +0100

    making some tests to check if correct positions and correct characters are calculated correctly

--- part_1/src/tests/test_mastermind.py
+++ part_1/src/tests/test_mastermind.py
@@ -18,6 +18,39 @@ class Tests(unittest.TestCase):
         guess = ['W', 'B', 'Y', 'G']
         self.assertIsNotNone(self.mastermind.evaluate_guess(guess))
 
+    def test_eval_guess_correct_position_and_symbol(self):
+        guess = ['W', 'B', 'Y', 'G']  # All correct
+        correct_pos, correct_symbol = self.mastermind.evaluate_guess(guess)
+        self.assertEqual(correct_pos, 4)
+        self.assertEqual(correct_symbol, 0)
+
+    def test_eval_guess_partial_correct(self):
+        # All correct symbols but in wrong positions
+        guess = ['B', 'W', 'G', 'Y']
+        correct_pos, correct_symbol = self.mastermind.evaluate_guess(guess)
+        self.assertEqual(correct_pos, 0)
+        self.assertEqual(correct_symbol, 4)
+
+    def test_eval_guess_mixed(self):
+        # One correct position (W), one correct symbol but wrong position (G)
+        guess = ['W', 'R', 'G', 'R']
+        correct_pos, correct_symbol = self.mastermind.evaluate_guess(guess)
+        self.assertEqual(correct_pos, 1)
+        self.assertEqual(correct_symbol, 1)
+
+    def test_eval_guess_none_correct(self):
+        guess = ['R', 'R', 'R', 'R']  # None correct
+        correct_pos, correct_symbol = self.mastermind.evaluate_guess(guess)
+        self.assertEqual(correct_pos, 0)
+        self.assertEqual(correct_symbol, 0)
+
+    def test_brightspace_testcase(self):
+        self.mastermind.secret_code = ['B', 'B', 'B', 'Y']
+        guess = ['B', 'B', 'B', 'B']
+        correct_pos, correct_symbol = self.mastermind.evaluate_guess(guess)
+        self.assertEqual(correct_pos, 3)
+        self.assertEqual(correct_symbol, 0)
+
 
 if __name__ == "__main__":
     unittest.main()
