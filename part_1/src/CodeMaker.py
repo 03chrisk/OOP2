7521091384ffa3af57e188b2adcbca8d5e8d3511
03chrisk <chris.kobriger@gmail.com>
2023-12-13 20:21:53 +0100

    implementing symbols attribute

--- part_1/src/CodeMaker.py
+++ part_1/src/CodeMaker.py
@@ -5,6 +5,6 @@ sys.path.append(os.getcwd() + "/part_1/src/")
 
 class Codemaker():
     def __init__(self):
-        self.symbols = None
+        self.symbols = ["W", "B", "Y", "G", "R", "K"]
         self.code_length = None
         self.secret_code = None
