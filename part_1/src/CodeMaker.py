4dbac6c163c27113a0b8ade5ff84a9ade4bfdebe
03chrisk <chris.kobriger@gmail.com>
2023-12-13 21:00:39 +0100

    implementing generate secret code method

--- part_1/src/CodeMaker.py
+++ part_1/src/CodeMaker.py
@@ -1,5 +1,11 @@
+import random
+
+
 class Codemaker():
     def __init__(self):
         self.symbols = ["W", "B", "Y", "G", "R", "K"]
         self.code_length = 4
-        self.secret_code = None
+        self.secret_code = self.generate_code()
+
+    def generate_code(self):
+        return [random.choice(self.symbols) for _ in range(self.code_length)]
