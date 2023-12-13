020761df2502fd3e28dc0d12926a88a26b9ada74
03chrisk <chris.kobriger@gmail.com>
2023-12-13 16:19:56 +0100

    testing_attributes

--- part_1/src/mastermind.py
+++ part_1/src/mastermind.py
@@ -1,9 +1,19 @@
 
 class Mastermind:
 
-    def __init__(self):
+    def __init__(self, attempts):
         self._symbols = ["W", "B", "Y", "G", "R", "K"]
+        self._code_length = 4
+        self._num_attempts = attempts
 
     @property
     def symbols(self):
-        return self._symbols
+        pass
+
+    @property
+    def code_length(self):
+        pass
+
+    @property
+    def num_attempts(self):
+        pass
