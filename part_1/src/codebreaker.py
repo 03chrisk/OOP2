b587e98665852745d0854dc9b4b758e132a06be6
03chrisk <chris.kobriger@gmail.com>
2023-12-13 22:21:17 +0100

    converting input to uppercase characters

--- part_1/src/codebreaker.py
+++ part_1/src/codebreaker.py
@@ -6,6 +6,6 @@ sys.path.append(os.getcwd() + "/part_1/src/")
 class Codebreaker():
     def make_guess(self):
         while True:
-            guess = input()
+            guess = input().upper()
             if len(guess) == 4:
                 return guess
