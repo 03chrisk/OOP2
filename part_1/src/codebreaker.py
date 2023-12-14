740e4db93c41fd3e507ea2f6d1ce240f2c5cd0bb
03chrisk <chris.kobriger@gmail.com>
2023-12-14 21:02:10 +0100

    main implemented and added some promt text

--- part_1/src/codebreaker.py
+++ part_1/src/codebreaker.py
@@ -6,7 +6,11 @@ sys.path.append(os.getcwd() + "/part_1/src/")
 class Codebreaker():
     def make_guess(self):
         while True:
-            guess = input().upper()
-            if all(char in ["W", "B", "Y", "G", "R", "K"]
-                    for char in guess) and len(guess) == 4:
-                return guess
+            guess = input("Enter your guess from the following letters : \
+                W, B, Y, G, R, K. (e.g., WYBR): ").upper()
+            if all(char in ["W", "B",
+                            "Y", "G",
+                            "R", "K"] for char in guess) and len(guess) == 4:
+                return list(guess)
+            print("Invalid input. Please use only W, B, Y, G, R, K and ensure\
+                 the guess is 4 symbols long.")
