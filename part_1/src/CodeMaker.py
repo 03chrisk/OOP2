f38990438af1911cbd224ec7bbaac22207ba560d
03chrisk <chris.kobriger@gmail.com>
2023-12-18 15:38:30 +0100

    part1 should be done

--- part_1/src/CodeMaker.py
+++ part_1/src/CodeMaker.py
@@ -5,7 +5,7 @@ class Codemaker():
     def __init__(self) -> None:
         self.__symbols = ["W", "B", "Y", "G", "R", "K"]
         self.__code_length = 4
-        self._secret_code = self.generate_code()
+        self.__secret_code = self.__generate_code()
 
     @property
     def symbols(self):
@@ -17,20 +17,20 @@ class Codemaker():
 
     @property
     def secret_code(self):
-        return self._secret_code
+        return self.__secret_code
 
     @secret_code.setter
     def secret_code(self, new_code):
         if isinstance(new_code, list) and len(new_code) == self.code_length:
             if all(symbol in self.__symbols for symbol in new_code):
-                self._secret_code = new_code
+                self.__secret_code = new_code
             else:
                 raise ValueError("New code contains invalid symbols.")
         else:
             raise ValueError("New code must be a list of length {}"
                              .format(self.__code_length))
 
-    def generate_code(self) -> list:
+    def __generate_code(self) -> list:
 
         """
         Randomly generates a code by using only the colours specified in
